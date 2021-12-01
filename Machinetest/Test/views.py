6aa2ef3ee62bb7667ed2aa07722e390c92b415b1
desaiakshay92 <desaiakshay92@yahoo.com>
2021-12-01 17:44:51 +0530

    Added Permission to View Projects

--- Machinetest/Test/views.py
+++ Machinetest/Test/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from Test.models import Client,Project,User
 from Test.serializer import ClientSerializer, ProjectSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 
 # Create your views here.
 class ClientViewset(viewsets.ModelViewSet):
@@ -12,6 +13,17 @@ class ProjectViewset(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+    permission_classes_by_action = {'list':[IsAdminUser]}
+    
+    def list(self, request, *args, **kwargs):
+        return super(ProjectViewset, self).list(request,*args, **kwargs)
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
+            
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
